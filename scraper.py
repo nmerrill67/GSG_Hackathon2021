0078f2f31f125c9a53d2784993744242126f906d
Kyle Wang <kylewang@udel.edu>
2021-04-24 21:32:53 -0400

    create file for scrapping BTC/ETH/DOGE data

--- scraper.py
+++ scraper.py
@@ -6,7 +6,27 @@ import json
 import datetime
 import csv
 import time
-filename = 'CryptoCurrency_Reddit042421.csv'
+
+limit_number_of_comments = None
+hot_limit = 1000
+
+# BTC
+# cryto = 'bitcoin'
+# cryto_short = 'btc'
+# filename = 'CryptoCurrency_Reddit042421_9pm_BTC.csv'
+
+# # ETH
+# cryto = 'ethereum'
+# cryto_short = 'eth'
+# filename = 'CryptoCurrency_Reddit042421_9pm_ETH.csv'
+
+# DOGE
+cryto = 'dogecoin'
+cryto_short = 'doge'
+filename = 'CryptoCurrency_Reddit042421_9pm_DOGE.csv'
+
+
+
 start = time.time()
 
 if __name__ == '__main__':
@@ -37,8 +57,8 @@ if __name__ == '__main__':
 
     with open(filename, mode='w') as cryoto_file:
         cryoto_writer = csv.writer(cryoto_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-
-        hot_posts = reddit.subreddit('CryptoCurrency').hot(limit=1000)
+        cryoto_writer.writerow(["UTC", "Post Date", "Number of Comments", "Upvote Ratio", "Text"])
+        hot_posts = reddit.subreddit('CryptoCurrency').hot(limit=hot_limit)
         i = 0
         for post in hot_posts:
         #print(post.title)
@@ -48,11 +68,22 @@ if __name__ == '__main__':
             i += 1
             post_time = datetime.datetime.fromtimestamp(post.created_utc)
             utc = post.created_utc
-            text = post.title
+            text = post.title.replace('\n', ' ') # save all text in one line
             num_comments = post.num_comments
             upvote_ratio = post.upvote_ratio
-            cryoto_writer.writerow([utc, post_time, num_comments, upvote_ratio, text])
 
-    print("Number of posts:", i)
+            # Filter:
+            if cryto in text.lower() or cryto_short in text.lower():
+                cryoto_writer.writerow([utc, post_time, num_comments, upvote_ratio, text])
+                post.comments.replace_more(limit=limit_number_of_comments)
+                for comment in post.comments.list():
+                    comment_post_time = datetime.datetime.fromtimestamp(comment.created_utc)
+                    comment_utc = comment.created_utc
+                    comment_text = comment.body.replace('\n', ' ')
+                    comment_num_comments = len(comment.replies)
+                    comment_upvote_ratio = comment.score
+                    cryoto_writer.writerow([comment_utc, comment_post_time, comment_num_comments, comment_upvote_ratio, comment_text])
+
+        print("Number of posts:", i)
 
 print("TIME SPENT: "+ str(time.time()-start))
